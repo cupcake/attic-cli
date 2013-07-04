7589d48eb87a5948345a074450bec420635959f0
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2013-07-04 16:59:18 -0400

    event wrapper, also contains directory

--- src/watchevent.cpp
+++ src/watchevent.cpp
@@ -1,11 +1,14 @@
 #include "watchevent.h"
 
-WatchEvent::WatchEvent(inotify_event* event) {
+WatchEvent::WatchEvent(inotify_event* event,
+                       const std::string& directory) {
     // Copy event locally
     if(event) {
         event_ = *event;
         
     }
+    directory_ = directory;
+    is_file_ = false;
 }
 
 WatchEvent::~WatchEvent() {}
--- src/watchevent.h
+++ src/watchevent.h
@@ -2,15 +2,21 @@
 #define WATCHEVENT_H_
 #pragma once
 
+#include <string>
 #include <sys/inotify.h>
+
 class WatchEvent {
 public:
-    WatchEvent(inotify_event* event);
+    WatchEvent(inotify_event* event,
+               const std::string& directory);
     ~WatchEvent();
 
-    inotify_event* event() { return &event_; }
+    const inotify_event* event() const { return &event_; }
+    const std::string& directory() const { return directory_; }
 private:
+    std::string directory_;
     inotify_event event_;
+    bool is_file_;
 };
 
 
